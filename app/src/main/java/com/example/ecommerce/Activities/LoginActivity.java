bd24d89ef380f71ea2ec26fad7f1e2a0208c8f31
sangnguyen02 <ntsang101002@gmail.com>
2023-10-31 21:14:30 +0700

    CompleteLoginUIUXanditsfunction

---
+++ app/src/main/java/com/example/ecommerce/Activities/LoginActivity.java
@@ -0,0 +1,149 @@
+package com.example.ecommerce.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ecommerce.R;
+//import com.example.ecommerce.utils.AndroidUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+public class LoginActivity extends AppCompatActivity {
+
+    CountryCodePicker countryCodePicker;
+    String phoneNumber;
+    Long timeoutSeconds = 60L;
+    String verificationCode;
+    PhoneAuthProvider.ForceResendingToken  resendingToken;
+    EditText phoneInput, otpInput;
+    MaterialButton sendOTPBtn, loginBtn;
+    TextView resendOtpTextView;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        countryCodePicker = findViewById(R.id.login_countrycode);
+        phoneInput = findViewById(R.id.login_phone_number_input);
+        otpInput = findViewById(R.id.otpInput);
+        sendOTPBtn = findViewById(R.id.sendOTP);
+        loginBtn = findViewById(R.id.login_btn);
+        resendOtpTextView = findViewById(R.id.resend_otp_textview);
+        countryCodePicker.registerCarrierNumberEditText(phoneInput);
+        sendOTPBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!countryCodePicker.isValidFullNumber()) {
+                    phoneInput.setError("Phone number not valid");
+                }
+                phoneNumber = countryCodePicker.getFullNumberWithPlus();
+                sendOtp(phoneNumber, true);
+
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String enteredOtp  = otpInput.getText().toString();
+                if (verificationCode != null && !verificationCode.isEmpty()) {
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCode, enteredOtp);
+                    signIn(credential);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Verification code is invalid", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    void sendOtp(String phoneNumber,boolean isResend){
+        startResendTimer();
+        PhoneAuthOptions.Builder builder =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)
+                        .setTimeout(timeoutSeconds, TimeUnit.SECONDS)
+                        .setActivity(this)
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                signIn(phoneAuthCredential);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                Toast.makeText(LoginActivity.this,"OTP verification failed",Toast.LENGTH_LONG);
+                                //AndroidUtil.showToast(getApplicationContext(),"OTP verification failed");
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(s, forceResendingToken);
+                                verificationCode = s;
+                                resendingToken = forceResendingToken;
+                                Toast.makeText(LoginActivity.this,"OTP sent successfully",Toast.LENGTH_LONG);
+                                //AndroidUtil.showToast(getApplicationContext(),"OTP sent successfully");
+                            }
+                        });
+        if(isResend){
+            PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
+        }else{
+            PhoneAuthProvider.verifyPhoneNumber(builder.build());
+        }
+    }
+
+    void signIn(PhoneAuthCredential phoneAuthCredential){
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    intent.putExtra("phone",phoneNumber);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"OTP verification failed",Toast.LENGTH_LONG);
+                    //AndroidUtil.showToast(getApplicationContext(),"OTP verification failed");
+                }
+            }
+        });
+    }
+    void startResendTimer(){
+        resendOtpTextView.setEnabled(false);
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                timeoutSeconds--;
+                resendOtpTextView.setText("Resend OTP in "+timeoutSeconds +" seconds");
+                if(timeoutSeconds<=0){
+                    timeoutSeconds =60L;
+                    timer.cancel();
+                    runOnUiThread(() -> {
+                        resendOtpTextView.setEnabled(true);
+                    });
+                }
+            }
+        },0,1000);
+    }
+}
---
+++ app/src/main/java/com/example/ecommerce/Activities/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.ecommerce.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.ecommerce.Fragments.HistoryFragment;
+import com.example.ecommerce.Fragments.HomeFragment;
+import com.example.ecommerce.Fragments.PaymentFragment;
+import com.example.ecommerce.Fragments.ProfileFragment;
+import com.example.ecommerce.R;
+import com.example.ecommerce.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(R.layout.activity_login);
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+
+
+                case R.id.history:
+                    replaceFragment(new HistoryFragment());
+                    break;
+
+
+                case R.id.payment:
+                    replaceFragment(new PaymentFragment());
+                    break;
+
+
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+
+
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
