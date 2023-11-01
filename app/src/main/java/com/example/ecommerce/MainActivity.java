38e834f09693ac2b3f1ebf361d6ead56af0d7bf7
Kayle Maximus <kaylemaximus@gmail.com>
2023-11-01 10:39:47 +0700

    adding zalo pay

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -1,14 +1,131 @@
 package com.example.ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+
+import model.CreateOrder;
+import vn.zalopay.sdk.Environment;
+import vn.zalopay.sdk.ZaloPayError;
+import vn.zalopay.sdk.ZaloPaySDK;
+import vn.zalopay.sdk.listeners.PayOrderListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TextView txttongtien, txtsodt,txtemail;
+    EditText edtdiachi;
+    AppCompatButton btnZalo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Zalo init
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        ZaloPaySDK.init(2553, Environment.SANDBOX);
+
+        initView();
+        initControl();
+    }
+
+    private void initControl() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+        long tongtien = 25000;
+        txttongtien.setText(decimalFormat.format(tongtien));
+        txtemail.setText("kyle@gmail.com");
+        txtsodt.setText("0924993544");
+
+        btnZalo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str_diachi = edtdiachi.getText().toString().trim();
+                if(TextUtils.isEmpty(str_diachi)){
+                    Toast.makeText(getApplicationContext(), "Bạn chưa nhập địa chỉ", Toast.LENGTH_SHORT).show();
+                }else{
+                    // post data
+                    //Log.d("test", new Gson().toJson(Util));
+                    requestZalo();
+
+                }
+
+            }
+        });
+    }
+
+    private void requestZalo() {
+        CreateOrder orderApi = new CreateOrder();
+
+        try {
+            JSONObject data = orderApi.createOrder("100000");
+            String code = data.getString("return_code");
+            Log.d("test", code);
+            if (code.equals("1")) {
+                String token = data.getString("zp_trans_token");
+                Log.d("test", token);
+                ZaloPaySDK.getInstance().payOrder(MainActivity.this, token, "demozpdk://app", new PayOrderListener() {
+                    @Override
+                    public void onPaymentSucceeded(String s, String s1, String s2) {
+
+
+                    }
+
+                    @Override
+                    public void onPaymentCanceled(String s, String s1) {
+
+                    }
+
+                    @Override
+                    public void onPaymentError(ZaloPayError zaloPayError, String s, String s1) {
+
+                    }
+                });
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        txttongtien = findViewById(R.id.txttongtien);
+        txtsodt = findViewById(R.id.txtsodienthoai);
+        txtemail = findViewById(R.id.txtemail);
+        edtdiachi = findViewById(R.id.edtdiachi);
+        btnZalo = findViewById(R.id.btnzalopay);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        ZaloPaySDK.getInstance().onResult(intent);
     }
 }
