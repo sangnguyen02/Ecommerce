060580b0b471e05ed8cf0d26082ddb0fce8fbf42
sangnguyen02 <ntsang101002@gmail.com>
2023-11-01 20:35:39 +0700

    CompleteBannerAutorun

--- app/src/main/java/com/example/ecommerce/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/ecommerce/Fragments/HomeFragment.java
@@ -3,64 +3,91 @@ package com.example.ecommerce.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.ecommerce.Adapters.BannerViewPager2Adapter;
+import com.example.ecommerce.Animations.ZoomOutPageTransformer;
+import com.example.ecommerce.Models.ImageBanner;
 import com.example.ecommerce.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import me.relex.circleindicator.CircleIndicator3;
+
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ViewPager2 mViewPager2;
+    private CircleIndicator3 mCircleIndicator3;
+    private List<ImageBanner> mListImageBanner;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if(mViewPager2.getCurrentItem() == mListImageBanner.size() - 1) {
+                mViewPager2.setCurrentItem(0);
+            }
+            else {
+                mViewPager2.setCurrentItem(mViewPager2.getCurrentItem() + 1);
+            }
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        mViewPager2 = rootView.findViewById(R.id.view_pager_2);
+        mCircleIndicator3 = rootView.findViewById(R.id.circle_indicator_3);
+
+        mListImageBanner = getListImageBanner();
+        BannerViewPager2Adapter adapter = new BannerViewPager2Adapter(mListImageBanner);
+        mViewPager2.setAdapter(adapter);
+
+        mCircleIndicator3.setViewPager(mViewPager2);
+
+        mViewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mHandler.removeCallbacks(mRunnable);
+                mHandler.postDelayed(mRunnable,5000);
+            }
+        });
+
+        mViewPager2.setPageTransformer(new ZoomOutPageTransformer());
+
+        return rootView;
+    }
+
+    private List<ImageBanner> getListImageBanner() {
+        List<ImageBanner> list = new ArrayList<>();
+        list.add(new ImageBanner(R.drawable.banner_ggmap));
+        list.add(new ImageBanner(R.drawable.banner_vnpay));
+        list.add(new ImageBanner(R.drawable.banner_voucher));
+        list.add(new ImageBanner(R.drawable.banner_giveaway));
+
+        return list;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mHandler.removeCallbacks(mRunnable);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mHandler.postDelayed(mRunnable,5000);
     }
 }
