133be76e30aa2779de10a066ac8940e11ec55e65
PhanTanCuong <phantancuong2002@gmail.com>
2023-11-02 09:12:50 +0700

    Intergrating PayPal API (login PayPal error)

--- app/src/main/java/com/example/ecommerce/config/Config.java
+++ app/src/main/java/com/example/ecommerce/config/Config.java
@@ -1,6 +1,6 @@
 package com.example.ecommerce.config;
 
 public class Config {
-    public static final String PAYPAL_CLIENT_ID="Aea6FDgVuRS9eF7Fy8-n7nyGLJ8q1FG_Rp0pNSVJxbeqqXlyZZ0vWqXafi8l-Xp6p5UQW5hliUF_MmG9";
+    public static final String PAYPAL_CLIENT_ID="Acae8A_QDnkqmFjL8qTKnkEtp0vyOmTwvIKwnKyqSk9gNTDIps7Tr65oH1j2GrgEajm72WuhWk7ydARr";
 
 }
