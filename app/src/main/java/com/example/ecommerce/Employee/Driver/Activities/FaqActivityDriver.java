4f3dab8a87c0d511267dd5b49ec2c98c5e34c4ee
sangnguyen02 <ntsang101002@gmail.com>
2023-11-10 10:50:26 +0700

    Layout_History_ProfileDriver

---
+++ app/src/main/java/com/example/ecommerce/Employee/Driver/Activities/FaqActivityDriver.java
@@ -0,0 +1,16 @@
+package com.example.ecommerce.Employee.Driver.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecommerce.R;
+
+public class FaqActivityDriver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq_driver);
+    }
+}
