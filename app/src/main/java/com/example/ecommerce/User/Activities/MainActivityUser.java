c337699b5deaf6dc153718e7d1410b25047a0606
sangnguyen02 <ntsang101002@gmail.com>
2023-11-15 16:26:27 +0700

    AddLayout_SignInFirstTime

--- app/src/main/java/com/example/ecommerce/User/Activities/MainActivityUser.java
+++ app/src/main/java/com/example/ecommerce/User/Activities/MainActivityUser.java
@@ -17,7 +17,7 @@ import com.example.ecommerce.databinding.ActivityMainBinding;
 public class MainActivityUser extends AppCompatActivity {
 
     ActivityMainBinding binding;
-    String phone;
+    String phone, name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +27,7 @@ public class MainActivityUser extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             phone = extras.getString("phone_number");
+            name = extras.getString("user_name");
         }
         replaceFragment(new HomeFragmentUser());
 
@@ -52,6 +53,7 @@ public class MainActivityUser extends AppCompatActivity {
                     ProfileFragmentUser profileFragmentUser = new ProfileFragmentUser();
                     Bundle bundle = new Bundle();
                     bundle.putString("phone_number", phone);
+                    bundle.putString("user_name", name);
                     profileFragmentUser.setArguments(bundle);
                     replaceFragment(profileFragmentUser);
                     break;
