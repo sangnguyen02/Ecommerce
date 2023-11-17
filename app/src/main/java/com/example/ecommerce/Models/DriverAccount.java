54aada99fed7e6e4a843990db05100e1874856e3
PhanTanCuong <phantancuong2002@gmail.com>
2023-11-17 18:19:53 +0700

    Added constructor for Driver Account model

--- app/src/main/java/com/example/ecommerce/Models/DriverAccount.java
+++ app/src/main/java/com/example/ecommerce/Models/DriverAccount.java
@@ -5,6 +5,54 @@ import android.location.Location;
 import com.example.ecommerce.Enum.MyEnum;
 
 public class DriverAccount {
+    public DriverAccount(String phoneNo, String mail, String password, Location currentLocation, MyEnum.DriverStatus driverStatus) {
+        this.phoneNo = phoneNo;
+        this.mail = mail;
+        this.password = password;
+        this.currentLocation = currentLocation;
+        this.driverStatus = driverStatus;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public MyEnum.DriverStatus getDriverStatus() {
+        return driverStatus;
+    }
+
+    public void setDriverStatus(MyEnum.DriverStatus driverStatus) {
+        this.driverStatus = driverStatus;
+    }
+
     private String phoneNo;
     private String mail;
     private String password;
