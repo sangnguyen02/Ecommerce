644c0c96bbcfe10d105b321846703019772a32ee
sangnguyen02 <ntsang101002@gmail.com>
2023-11-20 21:11:36 +0700

    SomeLayoutChanges_new

--- app/src/main/java/com/example/ecommerce/Employee/Driver/Fragments/HomeFragmentDriver.java
+++ app/src/main/java/com/example/ecommerce/Employee/Driver/Fragments/HomeFragmentDriver.java
@@ -2,6 +2,7 @@ package com.example.ecommerce.Employee.Driver.Fragments;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,13 +10,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ecommerce.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class HomeFragmentDriver extends Fragment {
-
+    CardView layoutBottomSheetRequestPopUp;
+    BottomSheetBehavior bottomSheetBehaviorRequestPopUp;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home_driver, container, false);
+        layoutBottomSheetRequestPopUp = rootView.findViewById(R.id.bottom_sheet_order_pop_up);
+        bottomSheetBehaviorRequestPopUp = BottomSheetBehavior.from(layoutBottomSheetRequestPopUp);
+        // bottomSheetBehaviorRequestPopUp.setState(BottomSheetBehavior.STATE_EXPANDED);
+        // bottomSheetBehaviorRequestPopUp.setState(BottomSheetBehavior.STATE_COLLAPSED);
 
 
         return rootView;
