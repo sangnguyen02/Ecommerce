4ceb8cfbe868447ec0fa68f51dd5a64a582a7bac
PhanTanCuong <phantancuong2002@gmail.com>
2023-11-22 09:48:10 +0700

    Successfully intergrate Paypal

---
+++ app/src/main/java/com/example/ecommerce/User/Paypal/App.java
@@ -0,0 +1,25 @@
+package com.example.ecommerce.User.Paypal;
+
+import android.app.Application;
+
+import com.paypal.checkout.PayPalCheckout;
+import com.paypal.checkout.config.CheckoutConfig;
+import com.paypal.checkout.config.Environment;
+import com.paypal.checkout.createorder.CurrencyCode;
+import com.paypal.checkout.createorder.UserAction;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PayPalCheckout.setConfig(new CheckoutConfig(
+                this,
+                "AQLjybR6ovCpMh9MsXqlUVOjgfj8ZqUHfyE0xW6RHMdwFBhpRVwziolihVSBWjiZWNmKeg-fwrxGUSuI",
+                Environment.SANDBOX,
+                CurrencyCode.USD,
+                UserAction.PAY_NOW,
+                "com.example.ecommerce://paypalpay"
+        ));
+    }
+}
---
+++ app/src/main/java/com/example/ecommerce/User/VNPay/VNP_API.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce.User.VNPay;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ecommerce.R;
+
+public class VNP_API extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vnp_api);
+    }
+}
